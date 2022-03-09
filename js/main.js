b0b76d4726883dcc2b934765c3312fbcbda31f42
Otkirjon2001 <otkirsalimboyev@gmail.com>
2022-03-09 19:20:11 +0300

    Add other styles

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-const elSiteButton = document.querySelector(".site-header-toggle");
-const elSitePrice1 = document.querySelector(".price-1");
-const elSitePrice2 = document.querySelector(".price-2");
-const elSitePrice3 = document.querySelector(".price-3");
-elSiteButton.addEventListener("click", function () {
-  elSitePrice1.textContent = "199.99";
-  elSitePrice2.textContent = "249.99";
-  elSitePrice3.textContent = "399.99";
-});
+const elsSiteInput = document.querySelectorAll('[name = "switch-input-radio"]');
+const elsSitePricing = document.querySelectorAll(".price");
+
+elsSiteInput.forEach(function (elSiteinput) {
+  elSiteinput.addEventListener("change", function () {
+    const period = elSiteinput.value;
+
+    if (period === "annually") {
+      elsSitePricing.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest(".card-box-item").dataset.pricingAnnually;
+      });
+    } else {
+      elsSitePricing.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest(".card-box-item").dataset.pricingMonthly;
+      });
+
+    }
+  })
+})
