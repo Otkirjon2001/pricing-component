af872ab4f66c9da2f8e06252929dbcb84591c6ed
Otkirjon2001 <otkirsalimboyev@gmail.com>
2022-03-25 11:05:16 +0300

    Add other styles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 const elsSiteInput = document.querySelectorAll('[name = "switch-input-radio"]');
 const elsSitePricing = document.querySelectorAll(".price");
-
 elsSiteInput.forEach(function (elSiteinput) {
   elSiteinput.addEventListener("change", function () {
     const period = elSiteinput.value;
-
     if (period === "annually") {
       elsSitePricing.forEach(function (elPrice) {
         elPrice.textContent = elPrice.closest(".card-box-item").dataset.pricingAnnually;
