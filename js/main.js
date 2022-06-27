5737e928a720e634b464d04f65623b61dbf9b1ea
Otkirjon2001 <otkirsalimboyev@gmail.com>
2022-06-27 15:12:54 +0300

    Add BEM technology

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const elsSiteInput = document.querySelectorAll('[name = "switch-input-radio"]');
-const elsSitePricing = document.querySelectorAll(".price");
+const elsSitePricing = document.querySelectorAll(".price__value");
 elsSiteInput.forEach(function (elSiteinput) {
   elSiteinput.addEventListener("change", function () {
     const period = elSiteinput.value;
     if (period === "annually") {
       elsSitePricing.forEach(function (elPrice) {
-        elPrice.textContent = elPrice.closest(".card-box-item").dataset.pricingAnnually;
+        elPrice.textContent = elPrice.closest(".price").dataset.pricingAnnually;
       });
     } else {
       elsSitePricing.forEach(function (elPrice) {
-        elPrice.textContent = elPrice.closest(".card-box-item").dataset.pricingMonthly;
+        elPrice.textContent = elPrice.closest(".price").dataset.pricingMonthly;
       });
 
     }
